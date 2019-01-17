9bc07297994ae657f4ae2481c2da5cec7b1489fc
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-01-18 03:08:56 +0530

    created html for second file. data is geting saved on a json file.

--- src/app.js
+++ src/app.js
@@ -1,26 +1,51 @@
 const fs = require('fs');
 
-const app = (req, res) => {
-	let path = './public' + req.url;
-	if (req.url == '/') path = "./public/index.html";
-	sendResponse(res, path);
-};
+const send = function (res, content, statusCode = 200) {
+	res.statusCode = statusCode;
+	res.write(content);
+	res.end();
+}
 
 const sendResponse = function (res, path) {
+	let errorMsg = "Not Found";
 	fs.readFile(path, (err, data) => {
 		try {
-			res.statusCode = 200;
-			res.write(data);
-			res.end();
+			send(res, data);
 		}
 		catch (err) {
-			res.statusCode = 400;
-			res.write("Not Found");
-			res.end();
+			send(res, errorMsg, 404);
 		}
 	})
 }
 
+const getDetails = function (content) {
+	let details = content.split('&');
+	let comment = details[1].split('=')[1].split('+').join(' ').split('%0D%0A').join('\n');
+	let name = details[0].split('=')[1].split('+').join(' ').split('%0D%0A').join('\n');
+	let dateAndTime = new Date().toLocaleString();
+	return { dateAndTime, name, comment };
+}
+
+const app = (req, res) => {
+	let path = './public' + req.url;
+	if (req.url == '/') path = "./public/index.html";
+	if (req.url == '/handleComment') {
+		let content = "";
+		req.on('data', (chunk) => content += chunk)
+		req.on('end', () => {
+			let details = JSON.stringify(getDetails(content));
+			fs.appendFile("./public/comments.json", details, (err) => {
+				if (err) throw err;
+				console.log('The data was appended to file!');
+			});
+			res.write(JSON.stringify(details) + '\nData got submited.');
+			res.end();
+		})
+		return;
+	}
+	sendResponse(res, path);
+};
+
 // Export a function that can act as a handler
 
-module.exports = app;
+module.exports = { app };
