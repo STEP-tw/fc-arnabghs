e06e1b7eb93f1b31595b529b3194baef543cdc88
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-01-23 15:23:10 +0530

    Established connection between login and logged in page

--- src/app.js
+++ src/app.js
@@ -5,13 +5,19 @@ const WebFrame = require('./frameWork');
 const { getDetails } = require('./guestBook');
 
 const uppperPart = {
-	loginPage: `<form id="form" method="POST">
+	loggedInPage: `<form id="form" method="POST">
 					<div class="feedbackHeading"> Leave a Comment </div>
 					<div>Name : <input type="text" name="name" required></div>
 					comment : <textarea name="comment" form="form" rows="5" cols="40">  </textarea>
 					<div><input class="submitButton" type="submit" value="submit"></div>
 					<hr>
-					</form>`
+					</form>`,
+	loginPage: `<form id="form" action="/login" method="POST">
+	<div class="feedbackHeading"> Login to Comment </div>
+	<div>Name : <input type="text" name="name" required>
+	<input class="submitButton" type="submit" value="Login"></div>
+	<hr>
+	</form>`
 }
 
 const send = function (res, content, statusCode = 200) {
@@ -61,14 +67,31 @@ const getComments = function (req, res) {
 const getGuestPage = function (req, res) {
 	let path = './public' + req.url;
 	let page = fs.readFileSync(path, 'utf8');
-	page = page.replace("###FORM###", uppperPart.loginPage);
+	let logPart = "loginPage";
+	console.log(req.headers.cookie);
+	if (req.headers.cookie) logPart = "loggedInPage";
+	page = page.replace("###FORM###", uppperPart[logPart]);
 	send(res, page);
 }
+
+const logUserIn = function (req, res) {
+	let content = "";
+	req.on('data', (chunk) => content += chunk);
+	req.on('end', () => {
+		res.setHeader("Set-Cookie", `username=${content.split('=')[1]}`);
+		res.writeHead(302, {
+			Location: "/guestBook.html"
+		});
+		res.end();
+	})
+}
+
 
 const app = new WebFrame();
 app.post('/guestBook.html', addDataToGuestBook);
 app.get('/guestBook.html', getGuestPage);
 app.get("/comments", getComments);
+app.post("/login", logUserIn)
 app.use(provideData);
 const handleRequest = app.handleRequest.bind(app)
 
